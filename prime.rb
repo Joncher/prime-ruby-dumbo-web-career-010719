9e966a4b224e0abe256ed06046d65e9c1a72864e
John Crespi Oncher <johnoncher@gmail.com>
2018-12-28 22:38:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 def prime?(number)
   range = 2..number
-  if number !<= 1 && for prime in range; number % prime == 0 end
+  if number !< 1 && for prime in range; number % prime == 0 end
   end
 end
