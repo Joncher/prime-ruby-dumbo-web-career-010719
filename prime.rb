3c40e0744cee8e426f2f1593dc8397dcbe3bd71d
John Crespi Oncher <johnoncher@gmail.com>
2018-12-28 23:36:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,16 @@
 def prime?(number)
   range = 2..number
   
-  for i in range
-    if number % i == 0
-      return FALSE
-    end
-  end
   
   if number <= 1
     return FALSE
   end
     
+  for i in range
+    if number % i == 0
+      return FALSE
+    end
+  end
   
   return TRUE
   
